abeb1a051573891d7a4fb3e6abd91d634ab6bc18
Name <user@example.com>
2025-02-06 20:30:34 +0700

    2.12: The Phonebook step 7 and step 8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import persons from './services/persons'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 } 
-  ]) 
+  const [personsAll, setPersonsAll] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+
+
+  useEffect(() => {
+     persons.getAll().then((response) => {
+       setPersonsAll(response.data)
+     })
+  })
+  
 
   const handleClick = (e) => {
     e.preventDefault()
@@ -34,7 +38,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
+      {personsAll.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
     </div>
   )
 }
