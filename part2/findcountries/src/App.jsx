cae0f228e4103624c407e1065d7240c0c9fc8249
Name <user@example.com>
2025-02-17 09:50:21 +0700

    part2 2.20 step 3

--- part2/findcountries/src/App.jsx
+++ part2/findcountries/src/App.jsx
@@ -1,11 +1,15 @@
 import { useState, useEffect } from 'react';
 import getAllCountries from './services/countriesServices';
+import axios from 'axios';
 
 function App() {
   const [query, setQuery] = useState('');
   const [countries, setCountries] = useState([]);
   const [filteredCountries, setFilteredCountries] = useState([]);
   const [selectedCountry, setSelectedCountry] = useState(null);
+  const [weather, setWeather] = useState(null);
+
+  const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
 
   useEffect(() => {
     getAllCountries().then((allCountries) => {
@@ -26,13 +30,31 @@ function App() {
     }
   }, [query, countries]);
 
+  useEffect(() => {
+    if (selectedCountry) {
+      const capital = selectedCountry.capital?.[0];
+      if (capital) {
+        axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${apiKey}&units=metric`)
+          .then(data => {
+            setWeather({
+              temperature: data.main.temp,
+              condition: data.weather[0].description,
+              icon: `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+            });
+          })
+          .catch(error => console.error("Error fetching weather data:", error));
+      }
+    } else {
+      setWeather(null);
+    }
+  }, [selectedCountry, apiKey]);
+
   const handleShow = (country) => {
     setSelectedCountry(country);
   };
 
   return (
     <div>
-      <h1>Country Search</h1>
       <input 
         type="text" 
         value={query} 
@@ -61,6 +83,15 @@ function App() {
             ))}
           </ul>
           <img src={(selectedCountry || filteredCountries[0]).flags.png} alt={`Flag of ${(selectedCountry || filteredCountries[0]).name.common}`} width="150" />
+          
+          {weather && (
+            <div>
+              <h3>Weather in {selectedCountry.capital?.[0]}</h3>
+              <p>Temperature: {weather.temperature} °C</p>
+              <p>Condition: {weather.condition}</p>
+              <img src={weather.icon} alt="Weather icon" />
+            </div>
+          )}
         </div>
       )}
     </div>
