14290356ff47b0b2e70fe159fd20dd5403d01e03
Novian <razecoding@gmail.com>
2025-05-01 14:18:42 +0700

    change phonebook endpoint

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,25 +1,26 @@
-import axios from 'axios'
-const baseUrl = 'http://localhost:3000/persons'
+import axios from "axios";
+const baseUrl = "/api/persons";
 
 const getAll = () => {
-  return axios.get(baseUrl)
-}
+  return axios.get(baseUrl);
+};
 
-const create = newObject => {
-  return axios.post(baseUrl, newObject)
-}
+const create = (newObject) => {
+  return axios.post(baseUrl, newObject);
+};
 
 const update = (id, newObject) => {
-  return axios.put(`${baseUrl}/${id}`, newObject)
-}
+  return axios.put(`${baseUrl}/${id}`, newObject);
+};
 
 const remove = (id) => {
-  return axios.delete(`${baseUrl}/${id}`)
-}
+  return axios.delete(`${baseUrl}/${id}`);
+};
 
-export default { 
-  getAll: getAll, 
-  create: create, 
-  update: update, 
-  remove: remove
-}
+export default {
+  getAll: getAll,
+  create: create,
+  update: update,
+  remove: remove,
+};
+
